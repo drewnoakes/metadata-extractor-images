d8182556933716c7069f16e02e5ebec286022bb6
Drew Noakes <git@drewnoakes.com>
2022-04-04 08:17:05 +1000

    Reorder target frameworks
    Current output is for net45. Make it the default.

--- src/dotnet/MetadataExtractor.MediaLibraryProcessor/DotNet/DotNetRunner.cs
+++ src/dotnet/MetadataExtractor.MediaLibraryProcessor/DotNet/DotNetRunner.cs
@@ -15,7 +15,7 @@ namespace MetadataExtractor.MediaLibraryProcessor
 
     internal static class DotNetRunner
     {
-        // TODO we get different output for net45 / netcoreapp -- compare both?
+        // TODO we get different output for different .NET target frameworks -- compare across those too?
         // TODO port UnknownTagHandler from Java
 
         public static async Task RunAsync(string repoRoot)
