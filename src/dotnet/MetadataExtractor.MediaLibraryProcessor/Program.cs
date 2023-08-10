107047bc7c35a3244136408869d0480d7b88a3cf
Drew Noakes <git@drewnoakes.com>
2023-08-10 20:38:49 +1000

    Use top level statements and improve log output

--- src/dotnet/MetadataExtractor.MediaLibraryProcessor/Program.cs
+++ src/dotnet/MetadataExtractor.MediaLibraryProcessor/Program.cs
@@ -1,26 +1,26 @@
 ﻿// Copyright (c) Drew Noakes and contributors. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using MetadataExtractor.MediaLibraryProcessor;
+
 using System;
 using System.IO;
 using System.Threading.Tasks;
 
-namespace MetadataExtractor.MediaLibraryProcessor;
+// TODO support building libraries from source too
 
-internal static class Program
-{
-    // TODO support building libraries from source too
+// TODO more robust way of finding repo root path
+string repoRoot = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, """..\..\..\..\..\.."""));
 
-    private static async Task Main()
-    {
-        // TODO more robust way of finding repo root path
-        var repoRoot = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\..\\..\\.."));
+// Update all .NET and Java metadata files
+await Console.Out.WriteLineAsync("Generating metadata files for both Java and .NET implementations...");
 
-        // Update all .NET and Java metadata files
-        await Task.WhenAll(
-            DotNetRunner.RunAsync(repoRoot),
-            JavaRunner.RunAsync(repoRoot));
+await Task.WhenAll(
+    DotNetRunner.RunAsync(repoRoot),
+    JavaRunner.RunAsync(repoRoot));
 
-        // Update comparisons between .NET and Java outputs
-        await DiffRunner.RunAsync(repoRoot);
-    }
-}
+// Update comparisons between .NET and Java outputs
+await Console.Out.WriteLineAsync("Generating metadata diff files...");
+
+await DiffRunner.RunAsync(repoRoot);
+
+await Console.Out.WriteLineAsync("Done.");
